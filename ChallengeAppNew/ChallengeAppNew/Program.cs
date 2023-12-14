8043faa95678c8f6381be53bc53d1c364c830847
Jacek Zybaczynski <900zyb@gmail.com>
2023-12-14 00:49:55 +0000

    ZADANIE DOMOWE DZIEŃ 4

--- ChallengeAppNew/ChallengeAppNew/Program.cs
+++ ChallengeAppNew/ChallengeAppNew/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Jedziemy z tematem!");
+﻿string name = "Jacek";
+int age = 33;
+char sex = 'M';
+
+if(name == "Jacek" && age == 33 &&  sex == 'M')
+{
+    Console.WriteLine("Przystojny mężczyzna w wieku 33 lat o niebagatelnym imieniu Jacek");
+}
+else if(name == "Ewa" && age < 30 && sex != 'M')
+{
+    Console.WriteLine("Kobieta o imieniu Ewa, wiek: Ponizej 30 lat");
+}
+else if(age < 18 && sex == 'M')
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
