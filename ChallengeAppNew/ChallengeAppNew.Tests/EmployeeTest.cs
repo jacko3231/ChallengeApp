22a8b444e517d0a1a2cf4158e7bd64aaa14feb2a
Jacek Zybaczynski <900zyb@gmail.com>
2024-01-06 20:46:58 +0000

    Zadanie domowe dzień 9

--- ChallengeAppNew/ChallengeAppNew.Tests/EmployeeTest.cs
+++ ChallengeAppNew/ChallengeAppNew.Tests/EmployeeTest.cs
@@ -3,20 +3,6 @@ namespace ChallengeAppNew.Tests
     public class EmployeeTests
     {
         [Test]
-        public void CompareAndCheckingStatisticsAreEqual()
-        {
-            var employee = new Employee("Jacek", "Zybaczynski");
-            employee.AddScore(4);
-            employee.AddScore(4);
-            employee.AddScore(1);
-
-            var statistics = employee.GetStatictics();
-
-            Assert.AreEqual(3, statistics.Average);
-            Assert.AreEqual(4, statistics.Max);
-            Assert.AreEqual(1, statistics.Min);
-        }
-        [Test]
         public void CompareAndCheckingStatisticsAreEqualOrNotEqual()
         {
             var employee = new Employee("Jacek", "Zybaczynski");
