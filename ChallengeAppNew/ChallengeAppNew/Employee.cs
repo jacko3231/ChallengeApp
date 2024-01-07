3318f3192935c2430957a1c266183a4f16bfbce9
Jacek Zybaczynski <900zyb@gmail.com>
2024-01-07 01:51:49 +0000

    Zadanie domowe dzień 10

--- ChallengeAppNew/ChallengeAppNew/Employee.cs
+++ ChallengeAppNew/ChallengeAppNew/Employee.cs
@@ -11,9 +11,42 @@
         }      
         public string Name { get; private set; }
         public string Surname { get; private set; }
-        public void AddScore(float number)
+        public void AddScore(float score)
         {
-            this.scores.Add(number);
+            if(score >= 0 && score <= 100)
+            {
+              this.scores.Add(score);
+            }
+            else
+            {
+                Console.WriteLine("Invalid score data");
+            }       
+        }
+        public void AddScore(string score)
+        {
+            if(float.TryParse(score, out float result))
+            {
+                this.AddScore(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+        public void Addscore(int score)
+        {
+            float scoreToInt = (int)score;
+            this.AddScore(scoreToInt);
+        }   
+        public void Addscore(double score)
+        {
+            float scoreToFloat = (float)score;
+            this.AddScore(scoreToFloat);
+        }
+        public void Addscore(long score)
+        {
+            float scoreToLong = (long)score;
+            this.AddScore(scoreToLong);
         }
         public Statistics GetStatictics()
         {
--- ChallengeAppNew/ChallengeAppNew/Program.cs
+++ ChallengeAppNew/ChallengeAppNew/Program.cs
@@ -1,12 +1,16 @@
 ﻿using ChallengeAppNew;
 
 Employee employee = new Employee("Jacek", "Zybaczyński");
-employee.AddScore(9);
+employee.AddScore("1");
 employee.AddScore(5);
+employee.AddScore(700);
+employee.AddScore("Jacek");
+employee.AddScore(8);
 employee.AddScore(1);
+employee.AddScore("80");
 var statistics = employee.GetStatictics();
 
 Console.WriteLine($"Min number: {statistics.Min}");
 Console.WriteLine($"Max number: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Average}");
+Console.WriteLine($"Average: {statistics.Average:N2}");
 
