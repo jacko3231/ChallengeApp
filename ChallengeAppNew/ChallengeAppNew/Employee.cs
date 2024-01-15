0865eebb014870ebfae653cb045f01437cb0cbbf
Jacek Zybaczynski <900zyb@gmail.com>
2024-01-15 02:08:03 +0000

    ZADANIE DOMOWE DZIEŃ 11

--- ChallengeAppNew/ChallengeAppNew/Employee.cs
+++ ChallengeAppNew/ChallengeAppNew/Employee.cs
@@ -1,32 +1,34 @@
-﻿namespace ChallengeAppNew
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace ChallengeAppNew
 {
     public class Employee
     {
         private List<float> scores = new List<float>();
-      
+
         public Employee(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
-        }      
+        }
         public string Name { get; private set; }
         public string Surname { get; private set; }
-       
+
         public void AddScore(float score)
         {
-            if(score >= 0 && score <= 100)
+            if (score >= 0 && score <= 100)
             {
-              this.scores.Add(score);
+                this.scores.Add(score);
             }
             else
             {
                 Console.WriteLine("Invalid score data");
-            }       
+            }
         }
-       
+
         public void AddScore(string score)
         {
-            if(float.TryParse(score, out float result))
+            if (float.TryParse(score, out float result))
             {
                 this.AddScore(result);
             }
@@ -35,39 +37,107 @@
                 Console.WriteLine("String is not float");
             }
         }
-        
+
         public void AddScore(int score)
         {
             float scoreToInt = (int)score;
             this.AddScore(scoreToInt);
-        }   
-        
+        }
+
         public void AddScore(double score)
         {
             float scoreToFloat = (float)score;
             this.AddScore(scoreToFloat);
         }
-        
+
         public void AddScore(long score)
         {
             float scoreToLong = (long)score;
             this.AddScore(scoreToLong);
         }
-       
-        public Statistics GetStatictics()
+        //Pętla Foreach
+        public Statistics GetStatistics()
         {
             var statictics = new Statistics();
             statictics.Average = 0;
             statictics.Max = float.MinValue;
             statictics.Min = float.MaxValue;
 
-            foreach(var score in this.scores)
+            foreach (var score in this.scores)
             {
                 statictics.Max = Math.Max(statictics.Max, score);
                 statictics.Min = Math.Min(statictics.Min, score);
                 statictics.Average += score;
             }
+            statictics.Average /= this.scores.Count;
 
+            return statictics;
+        }
+
+        //Pętla For
+        public Statistics GetStatisticsWithFor()
+        {
+            var statictics = new Statistics();
+            statictics.Average = 0;
+            statictics.Max = float.MinValue;
+            statictics.Min = float.MaxValue;
+            var index = 0;
+            index = scores.Count;
+            index++;
+
+            for (index = 0; index < scores.Count; index++)
+            {
+                statictics.Max = Math.Max(statictics.Max, this.scores[index]);
+                statictics.Min = Math.Min(statictics.Min, this.scores[index]);
+                statictics.Average += this.scores[index];
+                index++;
+            }
+
+            statictics.Average /= this.scores.Count;
+
+            return statictics;
+        }
+
+        //Pętla Do While
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statictics = new Statistics();
+            statictics.Average = 0;
+            statictics.Max = float.MinValue;
+            statictics.Min = float.MaxValue;
+            var index = 0;
+
+            do
+            {
+                statictics.Max = Math.Max(statictics.Max, this.scores[index]);
+                statictics.Min = Math.Min(statictics.Min, this.scores[index]);
+                statictics.Average += this.scores[index];
+                index++;
+
+            } while (index < this.scores.Count);
+                        
+            statictics.Average /= this.scores.Count;
+
+            return statictics;
+        }
+
+        //Pętla While
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statictics = new Statistics();
+            statictics.Average = 0;
+            statictics.Max = float.MinValue;
+            statictics.Min = float.MaxValue;
+            var index = 0;
+
+            while(index < this.scores.Count)
+            {
+                statictics.Max = Math.Max(statictics.Max, this.scores.Count);
+                statictics.Min = Math.Min(statictics.Min, this.scores.Count);
+                statictics.Average += this.scores.Count;
+                index++;
+            }
+   
             statictics.Average /= this.scores.Count;
 
             return statictics;
--- ChallengeAppNew/ChallengeAppNew/Program.cs
+++ ChallengeAppNew/ChallengeAppNew/Program.cs
@@ -10,9 +10,34 @@ employee.AddScore(8);
 employee.AddScore(54);
 employee.AddScore(1);
 employee.AddScore("80");
-var statistics = employee.GetStatictics();
 
+var statistics = employee.GetStatistics();
+var statistics2 = employee.GetStatisticsWithDoWhile();
+var statistics3 = employee.GetStatisticsWithWhile();
+var statistics4 = employee.GetStatisticsWithFor(); 
+
+Console.WriteLine();
+Console.WriteLine("Pętla Foreach:");
+Console.WriteLine();
 Console.WriteLine($"Min number: {statistics.Min}");
 Console.WriteLine($"Max number: {statistics.Max}");
 Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine();
+Console.WriteLine("Pętla Do While:");
+Console.WriteLine();
+Console.WriteLine($"Min number: {statistics2.Min}");
+Console.WriteLine($"Max number: {statistics2.Max}");
+Console.WriteLine($"Average: {statistics2.Average:N2}");
+Console.WriteLine();
+Console.WriteLine("Pętla While:");
+Console.WriteLine();
+Console.WriteLine($"Min number: {statistics3.Min}");
+Console.WriteLine($"Max number: {statistics3.Max}");
+Console.WriteLine($"Average: {statistics3.Average:N2}");
+Console.WriteLine();
+Console.WriteLine("Pętla For: ");
+Console.WriteLine();
+Console.WriteLine($"Min number: {statistics4.Min}");
+Console.WriteLine($"Max number: {statistics4.Max}");
+Console.WriteLine($"Average: {statistics4.Average:N2}");
 
